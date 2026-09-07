64b36a809a01b674ef99f9796e89ac809ee41108
Tobias Fischer <tobias@fischer-boebing.de>
2026-09-07 16:43:32 -0300

    Add explicit euler

--- simulation.py
+++ simulation.py
@@ -7,12 +7,14 @@ G = 1.0 # gravitational constant / constante gravitacional
 m1 = 1.0 # mass 1
 m2 = 1.0 # mass 2
 M = m1 + m2 # Total mass / Masa total
+mu = (m1 * m2) / M
 
 # Start at perihelion on the x-axis / Inicio en el perihelio en el eje x
 r0 = np.array([1.0, 0.0]) # Initial position / Position initala
 v_kreis = np.sqrt(G * M / np.linalg.norm(r0)) # velocity for a circle / velocidad para un circulo
 # For a flatter ellipse, use e.g., 1.35 / Para una elipse más plana, usa p.ej. 1.35
 v0 = np.array([0.0, 1.1 * v_kreis])
+p0 = v0 * mu
 
 # --- Calculate orbital prameters / Calcular parámetros orbitales ---
 r0_norm = np.linalg.norm(r0)
@@ -24,6 +26,7 @@ h = r0[0] * v0[1] - r0[0] * v0[0] # momentum / momento
 epsilon = np.sqrt(1.0 - (h**2) / (G * M * a))
 
 T_orbit = 2.0 * np.pi * np.sqrt(a**3 / (G * M)) #orbital period / periodo orbital
+print(T_orbit)
 n = np.sqrt(G * M / a**2) # mean motion / movimiento medio
 
 # --- Calculate analytical solution / calcualr solución analitica ---
@@ -52,10 +55,33 @@ for i, t in enumerate(t_eval):
     r_analytical[i] = [x,y]
     
     
+# --- Calculate numerical solution using Euler explicit, implicit and symplectic
+
+delta_t = 0.1
+n_steps = int(T_orbit/delta_t)
+
+r_explicit = np.zeros((n_steps + 1, 2))
+p_explicit = np.zeros((n_steps + 1, 2))
+r_explicit[0] = r0
+p_explicit[0] = p0
+
+for i in range(n_steps):
+    r_old = r_explicit[i]
+    p_old = p_explicit[i]
+    
+    r_norm = np.linalg.norm(r_old)
+
+    F = -(G * M * mu) * r_old / (r_norm**3)
+
+    r_explicit[i+1] = r_old + delta_t * (p_old / mu)
+    p_explicit[i+1] = p_old + delta_t * F
+
+    
 # --- Visualización ---
 plt.figure(figsize=(8, 6))
-plt.plot(r_analytical[:, 0], r_analytical[:, 1], label="Analytische Lösung", color="black")
+plt.plot(r_analytical[:, 0], r_analytical[:, 1], label="analytical solution", color="black")
 plt.plot(0, 0, 'ro', label="Schwerpunkt")
+plt.plot(r_explicit[:, 0], r_explicit[:, 1], label="explicit Euler", color="blue")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title(f"Zweikörperproblem - Analytische Bahn (ε = {epsilon:.2f})")
